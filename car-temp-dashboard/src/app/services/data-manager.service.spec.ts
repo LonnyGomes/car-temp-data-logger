3d059db35027d910916df1c139a74998a8c0e21f
Lonny Gomes <code@lonnygomes.com>
2022-06-26 21:47:11 -0400

    feat: add loadFile data service

--- car-temp-dashboard/src/app/services/data-manager.service.spec.ts
+++ car-temp-dashboard/src/app/services/data-manager.service.spec.ts
@@ -4,6 +4,8 @@ import { DataManagerService } from './data-manager.service';
 
 describe('DataManagerService', () => {
   let service: DataManagerService;
+  const SAMPLE_CSV = './fixtures/sample.csv';
+  const SAMPLE_CSV_DATA_LEN = 4;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +14,17 @@ describe('DataManagerService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('loadFile', () => {
+    it('should be defined', () => {
+      expect(service.loadData).toBeTruthy();
+    });
+
+    it('should load CSV', async () => {
+      const data = await service.loadData(SAMPLE_CSV);
+      expect(data).toBeTruthy();
+      expect(data.length).toEqual(SAMPLE_CSV_DATA_LEN);
+    });
   });
 });
--- car-temp-dashboard/src/app/services/data-manager.service.ts
+++ car-temp-dashboard/src/app/services/data-manager.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { csv, DSVRowArray } from 'd3';
+import {
+  TemperatureCSVDataModel,
+  TemperatureDataModel,
+} from '../models/temperature-data.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataManagerService {
+  constructor() {}
 
-  constructor() { }
+  async loadData(filename: string) {
+    let results: TemperatureDataModel[] = [];
+    try {
+      const data: TemperatureCSVDataModel[] = (await csv(
+        filename
+      )) as TemperatureCSVDataModel[];
+
+      // remap data into proper types and values
+      results = data.map((curItem) => {
+        return {
+          date: new Date(curItem.date),
+          photocell: (Number(curItem.photocell) / 1000) * 100,
+          sensor_1: Number(curItem.sensor_1),
+          sensor_2: Number(curItem.sensor_2),
+        };
+      });
+    } catch (error: any) {
+      console.error('error: loadData', error.message);
+      throw error;
+    }
+
+    return results;
+  }
 }
